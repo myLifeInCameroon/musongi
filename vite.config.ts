074dd23722541fa86e2263415a0652e0dcb93279
karlz-t <kkonarski42@gmail.com>
2025-12-10 13:58:11 +0100

    Deployment issues solving

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  build: {
+    outDir: "build",
+  },
   server: {
     host: "::",
     port: 8080,
